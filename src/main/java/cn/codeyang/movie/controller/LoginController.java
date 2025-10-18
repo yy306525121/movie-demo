d7ce0ff8b50b03d57e92a47b5ea9dd63f534c17b
yangzy <306525121@qq.com>
2025-10-18 15:22:16 +0800

    update

---
+++ src/main/java/cn/codeyang/movie/controller/LoginController.java
@@ -0,0 +1,14 @@
+package cn.codeyang.movie.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/movie-plus")
+public class LoginController {
+    @GetMapping()
+    public String login() {
+        return "login";
+    }
+}
